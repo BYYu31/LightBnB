59959a7e75156b6e1e9bb26c3129c4046a6341f2
Bo Yang Yu <boyangyu93@gmail.com>
2023-08-04 23:56:14 -0400

    create queries

---
+++ 1_queries/05_reservation_for_user.sql
@@ -0,0 +1,8 @@
+SELECT reservations.id as id, properties.title as title, properties.cost_per_night as cost_per_night, reservations.start_date as start_date, avg(property_reviews.rating) as average_rating
+FROM property_reviews
+JOIN reservations ON reservations.id = property_reviews.reservation_id
+JOIN properties ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY reservations.id, properties.title, properties.cost_per_night
+ORDER BY start_date ASC
+LIMIT 10;
